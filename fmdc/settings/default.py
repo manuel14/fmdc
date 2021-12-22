b9f45d94e8ad472b1a2234c059b40f2604c714ea
manuel zubieta <mzubieta@paypal.com>
2021-12-22 10:35:46 -0300

    Removing context proccesors from setttings

--- fmdc/settings/default.py
+++ fmdc/settings/default.py
@@ -29,7 +29,6 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'web.context_processors.show_revistas'
             ],
         },
     },
