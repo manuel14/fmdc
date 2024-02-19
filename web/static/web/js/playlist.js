c7d4eb556ff87a5efd09a2ececad46e31037c653
manuel matias zubieta <manuelzubieta14@gmail.com>
2024-02-19 15:27:27 -0300

    Fixes for playlist script, avoid multiple plays

--- web/static/web/js/playlist.js
+++ web/static/web/js/playlist.js
@@ -1,40 +1,87 @@
-$(document).ready(function($) {
-	var supportsAudio = !!document.createElement('audio').canPlayType;
+const audioElements = document.querySelectorAll('audio');
+const playAllButton = document.querySelector(".playAll");
 
-	var getAudio = function(index,tracks) {
-		return tracks[index];
-	}
+let currentlyPlaying = null;
 
-	var player = function(albumId) {
-		if (supportsAudio) {
-			var index = 0,
-				playing = false,
-				tracks = $('#'+albumId+'>audio'),
-				trackCount = tracks.length;
-			$.each(tracks, function(index, val) {
-				$(this).unbind('play').unbind('pause').unbind('ended');
-				$(this).bind('play', function() {
-					playing = true;
-				}).bind('pause', function() {
-					playing = false;
-				}).bind('ended', function() {
-					if ((index + 1) < trackCount) {
-						index++;
-						audio = getAudio(index,tracks);
-						audio.play();
-					} else {
-						playing = false;
-						index = 0;
-					}
-				});
-			});
-			audio = getAudio(index,tracks);
-			audio.play();		
-		}	
-	}
-	$('.playAll').click(function(e) {
-		e.preventDefault();
-		var albumId = $(this).attr('data-albumid');
-		player(albumId);
-	});
+// Function to stop the currently playing audio
+function stopCurrentlyPlaying() {
+    if (currentlyPlaying) {
+        currentlyPlaying.pause();
+        currentlyPlaying.currentTime = 0;
+    }
+}
+
+// Add click event listener to each audio element
+audioElements.forEach(audio => {
+    audio.addEventListener('play', () => {
+        if (audio !== currentlyPlaying) {
+            console.log("distinto")
+            
+            if (currentlyPlaying) {
+                currentlyPlaying.pause()
+                currentlyPlaying.currentTime = 0;
+            }
+            audio.play();
+            currentlyPlaying = audio;
+            
+        } else {
+            currentlyPlaying = null
+        }
+    });
+    
 });
+
+
+function player(albumId) {
+    console.log(albumId)
+    var supportsAudio = true; // Assuming this is defined elsewhere
+    if (supportsAudio) {
+        var index = 0,
+            playing = false,
+            tracks = document.querySelectorAll('#' + albumId + ' > audio'),
+            trackCount = tracks.length;
+
+        tracks.forEach(function(track, index) {
+            track.removeEventListener('play', onPlay);
+            track.removeEventListener('pause', onPause);
+            track.removeEventListener('ended', onEnded);
+
+            track.addEventListener('play', onPlay);
+            track.addEventListener('pause', onPause);
+            track.addEventListener('ended', onEnded);
+        });
+
+        function onPlay() {
+            playing = true;
+        }
+
+        function onPause() {
+            playing = false;
+        }
+
+        function onEnded() {
+            if ((index + 1) < trackCount) {
+                index++;
+                var audio = getAudio(index, tracks);
+                audio.play();
+            } else {
+                playing = false;
+                index = 0;
+            }
+        }
+
+        var audio = getAudio(index, tracks);
+        audio.play();
+    }
+}
+
+function getAudio(index, tracks) {
+    return tracks[index];
+}
+
+const playAll = document.querySelector('.playAll')
+playAll.addEventListener("click", (e) => {
+    e.preventDefault();
+    const albumId = playAll.dataset.albumid;
+    player(albumId)
+})
